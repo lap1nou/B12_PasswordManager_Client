5faab166a861efeae07b96cb3ae0853a607cf36f
Olivier Koffi <o.koffi021194@gmail.com>
2019-03-25 15:43:20 +0100

    First commit Project Structure

---
+++ src/test/java/ch/heigvd/pro/client/AES_EncryptionTest.java
@@ -0,0 +1,18 @@
+package ch.heigvd.pro.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AES_EncryptionTest {
+
+    @Test
+    void encrypt() {
+        System.out.println("test");
+    }
+
+    @Test
+    void decrypt() {
+        System.out.println("test");
+    }
+}
