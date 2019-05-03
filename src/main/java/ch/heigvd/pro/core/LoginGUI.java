f6689dd1f19c3fa619514ecf136487dd472c4445
silverkameni <silverkameni@yahoo.fr>
2019-05-03 03:08:48 +0200

    login local v1

--- src/main/java/ch/heigvd/pro/core/LoginGUI.java
+++ src/main/java/ch/heigvd/pro/core/LoginGUI.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
 public class LoginGUI extends JFrame {
     public JPanel mainPanel;
@@ -15,9 +16,17 @@ public class LoginGUI extends JFrame {
     private JButton loginButton;
     private JButton cancelButton;
     private JButton forgetPasswordButton;
-    private JFormattedTextField formattedTextField1;
-    private JPasswordField passwordField1;
-    private JLabel label;
+    private JFormattedTextField usernameTextField;
+    private JPasswordField passwordField;
+    private JLabel labelAccount;
+    private JLabel keyFileLabel;
+    private JTextField keyFileTextField;
+    private JLabel textSelection;
+    private JRadioButton usernamePasswordRadioButton;
+    private JRadioButton keyFileRadioButton;
+    private JLabel usernameLabel;
+    private JLabel masterFileldPassword;
+    private JButton browseButton;
 
     public LoginGUI() {
 
@@ -31,8 +40,12 @@ public class LoginGUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
+        ButtonGroup group = new ButtonGroup();
+        group.add(keyFileRadioButton);
+        group.add(usernamePasswordRadioButton);
+
         // Listeners
-        label.addMouseListener(new MouseAdapter() {
+        labelAccount.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
@@ -40,10 +53,44 @@ public class LoginGUI extends JFrame {
                 dispose();
             }
         });
+
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 HomePageGUI myHomePageGUI = new HomePageGUI();
                 dispose();
+            }
+        });
+
+
+        usernamePasswordRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                keyFileTextField.setEnabled(false);
+            }
+        });
+
+        keyFileRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                usernameTextField.setEnabled(false);
+                passwordField.setEnabled(false);
+                forgetPasswordButton.setEnabled(false);
+                labelAccount.setEnabled(false);
+                
+
+            }
+        });
+
+        browseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser(new File("c:\\"));
+                chooser.showOpenDialog(null);
+                // chooser.setFileFilter(new FileTypeFilter(".txt", ".docx", ".pdf"));
+                File f = chooser.getSelectedFile();
+                String filename = f.getAbsolutePath();
+                keyFileTextField.setText(filename);
+
             }
         });
     }
